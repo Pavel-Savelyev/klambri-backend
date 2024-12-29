e917ba4f6b8047f6812096ec51155d2ddc113e14
Pavel Savelyev <savpavel2000@gmail.com>
2024-12-29 22:48:42 +0500

    init configurator

--- cmd/main.go
+++ cmd/main.go
@@ -1,4 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"klambri-backend/internal/configuration"
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
+	config, err := configuration.ReadConfig("config/config.yaml")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	r := gin.Default()
+	if err := r.Run(fmt.Sprintf(":%v", config.Server.Port)); err != nil {
+		panic(err)
+	}
 }
